12c71b3923a0d4cfc98e211a77222129bb653d1b
Priyanshi Gupta <guptapriyanshi846@gmail.com>
2026-05-27 23:05:40 +0530

    Practiced python Day2

--- Python basics/Operators.py
+++ Python basics/Operators.py
@@ -1,5 +1,12 @@
 #Write a Python program that takes the base and height as input from the user and calculates the area of the triangle.
-base = float(input("Enter the base of the triangle:"))
-height = float(input("Enter the height of the triangle:"))
-area = (base*height)/2
-print("The area of the triangle is:", area)
+# base = float(input("Enter the base of the triangle:"))
+# height = float(input("Enter the height of the triangle:"))
+# area = (base*height)/2
+# print("The area of the triangle is:", area)
+
+a=10
+b=5
+print("a>b", a>b)
+print("a<b",a<b)
+print("a==b",a==b)
+print("a!=b",a!=b)
